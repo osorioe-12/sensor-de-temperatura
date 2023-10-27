8e10f5fb23a55629caaa11c8849a377624ab8893
efrai <efrai@OSORIO12>
2023-10-26 23:05:02 -0600

    Agregar archivos de proyecto.

---
+++ sensor de temperatura/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace sensor_de_temperatura
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(104, 241);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 66);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "CONECTAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(326, 241);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(149, 66);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "DESCONECTAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(495, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "TEMPERATURA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(201, 170);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(0, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 16);
+            this.label3.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(155, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(242, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "LIMITE DE TEMPERATURA";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(204, 95);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ sensor de temperatura/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;  
+
+namespace sensor_de_temperatura
+{
+    public partial class Form1 : Form
+    {
+        delegate void setTextDelegate(string value);
+        public SerialPort ArduinoPort
+        {
+            get;
+        }
+    public Form1()
+        {
+            InitializeComponent();
+            ArduinoPort = new System.IO.Ports.SerialPort();
+            ArduinoPort.PortName = "COM4";
+            ArduinoPort.BaudRate = 9600;
+            ArduinoPort.DataBits = 8;
+            ArduinoPort.ReadTimeout = 500;
+            ArduinoPort.WriteTimeout = 500;
+            ArduinoPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+            this.button1.Click += button1_Click;
+        }
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            string dato=ArduinoPort.ReadLine();
+            EscribirTxt(dato);
+
+        }
+        private void EscribirTxt(string dato)
+        {
+            if (InvokeRequired)
+                try
+                {
+                    Invoke(new setTextDelegate(EscribirTxt), dato);
+                }
+                catch { }
+            else 
+                label1.Text=dato;
+            
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            button2.Enabled = true;
+            try
+            {
+                if (!ArduinoPort.IsOpen)
+                    ArduinoPort.Open();
+                if (int.TryParse(label2.Text, out int temperatureLimit))
+                {
+                    string limitString = textBox1.ToString();
+                    ArduinoPort.Write(limitString);
+
+                }
+                else
+                {
+                    MessageBox.Show("Ingresa un valor numérico válido en el TextBox del limite de la temperatura.");
+
+                }
+                label1.Text = "Conexión OK";
+                label1.ForeColor = System.Drawing.Color.Lime;
+
+
+            }
+            catch
+            {
+                MessageBox.Show("Configure el puerto de comunicación correcto o Desconecte");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            button1.Enabled = true;
+            button2.Enabled = false;
+            if (ArduinoPort.IsOpen)
+                ArduinoPort.Close();
+            label2.Text = "Desconectado";
+            label2.ForeColor = System.Drawing.Color.Red;
+            label1.Text = "00.0";
+        }
+    }
+}
